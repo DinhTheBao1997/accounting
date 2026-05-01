e1ed9a8bf7900529e4a9778dcd9af9d4aa880bf2
dinhthebao <dinhthebao0706@gmail.com>
2026-05-02 06:25:55 +0700

    Remove unused class and configuration - I changed from idea auto parse excel to analyze insurance data

--- backend/src/main/java/com/vn/insurance/accounting/security/interfaces/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.vn.insurance.accounting.security.interfaces;
-
-import com.vn.insurance.accounting.security.model.User;
-
-public interface UserService {
-	User findByUsername(String username);
-}
--- backend/src/main/java/com/vn/insurance/accounting/utils/excel/interfaces/ExcelGridWriter.java
+++
@@ -1,8 +0,0 @@
-package com.vn.insurance.accounting.utils.excel.interfaces;
-
-import java.util.List;
-import java.util.Map;
-
-public interface ExcelGridWriter {
-	public int initGrid(List<Map<String, Object>> dataList);
-}
